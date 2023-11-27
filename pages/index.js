ecfd81f50ae1fa2dffb10610aee4558bba7a3b75
smuser90 <sam.musso90@gmail.com>
2023-11-26 20:50:15 -0700

    aider: Update homepage layout with personalized content and placeholders.
    # Aider chat conversation:
    
    ASSISTANT: Certainly! I'll provide you with a boilerplate layout for your homepage that you can customize with your own content later. We'll make the following changes:
    
    1. Update the page title to a generic "My Personal Blog".
    2. Change the main heading to a placeholder "Welcome to My Blog".
    3. Modify the description to a placeholder "Here I share my thoughts on software development, projects, and more."
    4. Simplify the grid to have placeholders for "Projects", "Blog", and "About Me".
    5. Update the footer to remove the Vercel branding and replace it with a generic placeholder.
    
    Here are the *SEARCH/REPLACE* blocks for these changes:
    
    pages/index.js
    ```jsx
    <<<<<<< SEARCH
            <title>Create Next App</title>
    =======
            <title>My Personal Blog</title>
    >>>>>>> REPLACE
    ```
    
    pages/index.js
    ```jsx
    <<<<<<< SEARCH
              Welcome to <a href="https://nextjs.org">Next.js!</a>
    =======
              Welcome to My Blog
    >>>>>>> REPLACE
    ```
    
    pages/index.js
    ```jsx
    <<<<<<< SEARCH
              Get started by editing <code>pages/index.js</code>
    =======
              Here I share my thoughts on software development, projects, and more.
    >>>>>>> REPLACE
    ```
    
    pages/index.js
    ```jsx
    <<<<<<< SEARCH
              <a href="https://nextjs.org/docs" className="card">
                <h3>Documentation &rarr;</h3>
                <p>Find in-depth information about Next.js features and API.</p>
              </a>
    
              <a href="https://nextjs.org/learn" className="card">
                <h3>Learn &rarr;</h3>
                <p>Learn about Next.js in an interactive course with quizzes!</p>
              </a>
    
              <a
                href="https://github.com/vercel/next.js/tree/master/examples"
                className="card"
              >
                <h3>Examples &rarr;</h3>
                <p>Discover and deploy boilerplate example Next.js projects.</p>
              </a>
    
              <a
                href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                className="card"
              >
                <h3>Deploy &rarr;</h3>
                <p>
                  Instantly deploy your Next.js site to a public URL with Vercel.
                </p>
              </a>
    =======
              <a href="#" className="card">
                <h3>Projects &rarr;</h3>
                <p>Explore my portfolio and recent work.</p>
              </a>
    
              <a href="#" className="card">
                <h3>Blog &rarr;</h3>
                <p>Read my latest articles and tutorials.</p>
              </a>
    
              <a href="#" className="card">
                <h3>About Me &rarr;</h3>
                <p>Learn more about my journey and experience.</p>
              </a>
    >>>>>>> REPLACE
    ```
    
    pages/index.js
    ```jsx
    <<<<<<< SEARCH
            <a
              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
              target="_blank"
              rel="noopener noreferrer"
            >
              Powered by{' '}
              <img src="/vercel.svg" alt="Vercel" className="logo" />
            </a>
    =======
            <footer>
              <p>© 2023 My Personal Blog. All rights reserved.</p>
            </footer>
    >>>>>>> REPLACE
    ```
    
    After applying these changes, you'll have a more personalized starting point for your blog. You can then fill in the specific text, links, and any additional content in your IDE. If you need further customization or additional features, feel free to ask!

--- pages/index.js
+++ pages/index.js
@@ -4,59 +4,41 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>My Personal Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to My Blog
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Here I share my thoughts on software development, projects, and more.
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className="card">
+            <h3>Projects &rarr;</h3>
+            <p>Explore my portfolio and recent work.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="#" className="card">
+            <h3>Blog &rarr;</h3>
+            <p>Read my latest articles and tutorials.</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="#" className="card">
+            <h3>About Me &rarr;</h3>
+            <p>Learn more about my journey and experience.</p>
           </a>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        <footer>
+          <p>© 2023 My Personal Blog. All rights reserved.</p>
+        </footer>
       </footer>
 
       <style jsx>{`
