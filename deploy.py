cbf1d7a542713431360849b8bbb040474b9a7090
smuser90 <sam.musso90@gmail.com>
2023-11-28 21:05:18 -0700

    aider: Added a `deploy.py` script that uses `boto3` to upload the contents of the `out/` directory to an S3 bucket using the default AWS profile.
    # Aider chat conversation:
    
    USER: write me a deploy.py that uses boto3 to push the contents of out/ to an s3 bucket using the default aws profile
    
    ASSISTANT: To create a `deploy.py` script that uses `boto3` to upload the contents of a directory `out/` to an S3 bucket, follow these steps:
    
    1. Ensure you have `boto3` installed in your Python environment. If not, you can install it using `pip install boto3`.
    2. Set up your AWS credentials in the default profile, typically located at `~/.aws/credentials`.
    3. Write the `deploy.py` script that will:
       - Import the necessary `boto3` module and other required modules.
       - Define a function to upload files to the specified S3 bucket.
       - Iterate over the files in the `out/` directory and upload each file.
       - Handle any exceptions that may occur during the upload process.
    
    Here is the `deploy.py` script:
    
    deploy.py
    ```python
    <<<<<<< SEARCH
    =======
    import os
    import boto3
    from botocore.exceptions import NoCredentialsError
    
    def upload_files_to_s3(bucket_name, directory):
        """
        Uploads the contents of the specified directory to the given S3 bucket.
        :param bucket_name: Name of the S3 bucket to upload files to.
        :param directory: Directory containing the files to upload.
        """
        # Create an S3 client
        s3_client = boto3.client('s3')
    
        # Walk through the directory
        for subdir, dirs, files in os.walk(directory):
            for file in files:
                full_path = os.path.join(subdir, file)
                with open(full_path, 'rb') as data:
                    try:
                        s3_client.upload_fileobj(data, bucket_name, full_path[len(directory)+1:])
                        print(f"File {full_path} uploaded to {bucket_name}")
                    except NoCredentialsError:
                        print("Credentials not available")
                    except Exception as e:
                        print(f"Failed to upload {full_path}: {e}")
    
    if __name__ == '__main__':
        # Define the name of your S3 bucket
        bucket_name = 'your-bucket-name'
    
        # Define the directory to upload
        directory_to_upload = 'out/'
    
        # Call the function to upload the directory contents to S3
        upload_files_to_s3(bucket_name, directory_to_upload)
    >>>>>>> REPLACE
    ```
    
    Please replace `'your-bucket-name'` with the actual name of your S3 bucket before running the script. Also, ensure that the AWS credentials you are using have the necessary permissions to write to the specified S3 bucket.

---
+++ deploy.py
@@ -0,0 +1,35 @@
+import os
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+def upload_files_to_s3(bucket_name, directory):
+    """
+    Uploads the contents of the specified directory to the given S3 bucket.
+    :param bucket_name: Name of the S3 bucket to upload files to.
+    :param directory: Directory containing the files to upload.
+    """
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+    
+    # Walk through the directory
+    for subdir, dirs, files in os.walk(directory):
+        for file in files:
+            full_path = os.path.join(subdir, file)
+            with open(full_path, 'rb') as data:
+                try:
+                    s3_client.upload_fileobj(data, bucket_name, full_path[len(directory)+1:])
+                    print(f"File {full_path} uploaded to {bucket_name}")
+                except NoCredentialsError:
+                    print("Credentials not available")
+                except Exception as e:
+                    print(f"Failed to upload {full_path}: {e}")
+
+if __name__ == '__main__':
+    # Define the name of your S3 bucket
+    bucket_name = 'your-bucket-name'
+    
+    # Define the directory to upload
+    directory_to_upload = 'out/'
+    
+    # Call the function to upload the directory contents to S3
+    upload_files_to_s3(bucket_name, directory_to_upload)
