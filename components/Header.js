855c448e6ce0c86b221d66c971f88ef774751e97
smuser90 <sam.musso90@gmail.com>
2023-11-29 15:07:52 -0700

    Added a new component for the header with social media icons.

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import Head from 'next/head'
+
+const Header = () => (
+  <header className="header" style={{ position: 'absolute', top: 0, zIndex: 1000 }}>
+    <Head>
+      <title>Sam Musso</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1 className="title">
+      SM Userspace {"</>"}
+    </h1>
+
+    <div className="social-icons">
+      <a href="https://github.com/smuser90" target="_blank" rel="noopener noreferrer" className="icon">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a href="https://linkedin.com/in/sammusso" target="_blank" rel="noopener noreferrer" className="icon">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href="https://stackoverflow.com/users/4312368/sam-musso" target="_blank" rel="noopener noreferrer" className="icon">
+        <FontAwesomeIcon icon={faStackOverflow} />
+      </a>
+    </div>
+  </header>
+);
+
+export default Header;
