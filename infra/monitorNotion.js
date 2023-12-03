e1e4881a804d0d05bb06ba13f45b907c5e4f46ae
smuser90 <sam.musso90@gmail.com>
2023-12-03 16:29:39 -0700

    aider: Add call to child_process.exec() in checkForChanges() function to run 'infra/exportNotion.js' when changes are detected.
    # Aider chat conversation:
    
    ASSISTANT: To make this change, we need to modify `infra/monitorNotion.js`:
    
    1. Import the child_process module to run the script.
    2. Add a call to child_process.exec() in the checkForChanges() function to run 'infra/exportNotion.js' when changes are detected.
    
    Here is the *SEARCH/REPLACE* block:
    
    infra/monitorNotion.js
    ```javascript
    <<<<<<< SEARCH
    const path = require('path');
    =======
    const path = require('path');
    const { exec } = require('child_process');
    >>>>>>> REPLACE
    ```
    
    infra/monitorNotion.js
    ```javascript
    <<<<<<< SEARCH
                console.log("Notion changes detected");
                HASH_STASH = hash;
    =======
                console.log("Notion changes detected");
                exec('node infra/exportNotion.js', (error, stdout, stderr) => {
                    if (error) {
                        console.error(`exec error: ${error}`);
                        return;
                    }
                    console.log(`stdout: ${stdout}`);
                    console.error(`stderr: ${stderr}`);
                });
                HASH_STASH = hash;
    >>>>>>> REPLACE
    ```

--- infra/monitorNotion.js
+++ infra/monitorNotion.js
@@ -2,6 +2,7 @@ const CryptoJS = require("crypto-js");
 const { Client } = require('@notionhq/client');
 const fs = require('fs');
 const path = require('path');
+const { exec } = require('child_process');
 
 // Initialize Notion client
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
@@ -15,6 +16,14 @@ async function checkForChanges() {
         const hash = JSON.stringify(response.results);
         if (hash != HASH_STASH) {
             console.log("Notion changes detected");
+            exec('node infra/exportNotion.js', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+            });
             HASH_STASH = hash;
         }
     } catch (e) {
