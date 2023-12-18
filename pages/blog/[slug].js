1f41ffbc2a4b8e1a083f1682e56ea62ed89b4a69
smuser90 <sam.musso90@gmail.com>
2023-12-18 16:02:24 -0700

    aider: Fix handling of space character in filenames by encoding/decoding paths and filenames.
    # Aider chat conversation:
    
    ASSISTANT: To fix this issue, we need to modify the `getStaticPaths` and `getStaticProps` functions in the `pages/blog/[slug].js` file.
    
    The problem is that the space character in the filename is not being handled correctly when creating the paths and when reading the file. We can fix this by encoding the filename when creating the paths and decoding it when reading the file.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    pages/blog/[slug].js
    ```javascript
    <<<<<<< SEARCH
        const paths = filenames
            .filter(filename => path.extname(filename) === '.md')
            .map(filename => ({
                params: { slug: filename.replace(/\.md$/, '') },
            }));
    =======
        const paths = filenames
            .filter(filename => path.extname(filename) === '.md')
            .map(filename => ({
                params: { slug: encodeURIComponent(filename.replace(/\.md$/, '')) },
            }));
    >>>>>>> REPLACE
    ```
    
    pages/blog/[slug].js
    ```javascript
    <<<<<<< SEARCH
        const filePath = path.join(postsDirectory, `${params.slug}.mdx`);
    =======
        const filePath = path.join(postsDirectory, `${decodeURIComponent(params.slug)}.mdx`);
    >>>>>>> REPLACE
    ```

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
     const paths = filenames
         .filter(filename => path.extname(filename) === '.md')
         .map(filename => ({
-            params: { slug: filename.replace(/\.md$/, '') },
+            params: { slug: encodeURIComponent(filename.replace(/\.md$/, '')) },
         }));
 
     return { paths, fallback: false };
@@ -27,7 +27,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
     const postsDirectory = path.join(process.cwd(), 'pages/blog');
     console.log(params);
-    const filePath = path.join(postsDirectory, `${params.slug}.mdx`);
+    const filePath = path.join(postsDirectory, `${decodeURIComponent(params.slug)}.mdx`);
     const fileContents = fs.readFileSync(filePath, 'utf8');
 
     // Use remark or a similar library to convert Markdown to HTML
