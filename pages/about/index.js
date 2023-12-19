d7da12347d4cc23929b3a330726ff59b4c3f6a43
smuser90 <sam.musso90@gmail.com>
2023-12-19 16:11:33 -0700

    aider: Refactored the calculation of `opacity` in the `draw` function to increase by 10% every 25ms during the transition phase.
    # Aider chat conversation:
    
    ASSISTANT: To implement this change, we need to modify the calculation of `opacity` in the `draw` function in `pages/about/index.js`.
    
    Here is the *SEARCH/REPLACE* block:
    
    pages/about/index.js
    ```javascript
    <<<<<<< SEARCH
            if (timeElapsed < transitionDuration) {
              // we are in transition phase
              opacity = Math.pow(2, (-10 * timeElapsed) / transitionDuration);
            } else if (timeElapsed < transitionDuration + steadyStateDuration) {
              // we are in steady state phase
              opacity = 1;
            } else {
              // transition to the next image
              transitionStartTime = Date.now();
              activeImage = 1 - activeImage;
            }
    =======
            if (timeElapsed < transitionDuration) {
              // we are in transition phase
              opacity = 1 - Math.pow(0.9, timeElapsed * 1000 / 25);
            } else if (timeElapsed < transitionDuration + steadyStateDuration) {
              // we are in steady state phase
              opacity = 1;
            } else {
              // transition to the next image
              transitionStartTime = Date.now();
              activeImage = 1 - activeImage;
            }
    >>>>>>> REPLACE
    ```
    
    This change will make the opacity increase by 10% every 25ms during the transition phase.

--- pages/about/index.js
+++ pages/about/index.js
@@ -47,7 +47,7 @@ export default function About() {
 
         if (timeElapsed < transitionDuration) {
           // we are in transition phase
-          opacity = Math.pow(2, (-10 * timeElapsed) / transitionDuration);
+          opacity = 1 - Math.pow(0.9, timeElapsed * 1000 / 25);
         } else if (timeElapsed < transitionDuration + steadyStateDuration) {
           // we are in steady state phase
           opacity = 1;
